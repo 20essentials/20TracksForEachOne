3af8b28e6f69072bb70867d83a2ecd9c76119c78
DominicDev <s0l0estudi4r@gmail.com>
2025-08-28 23:02:42 +0200

    v1.4.6

--- script.js
+++ script.js
@@ -217,6 +217,7 @@ const bands = [
   ['Hiroyuki Sawano', `${path}/sawano-hiro/`],
   ['Hardwell', `${path}/hardwell-band/`],
   ['Him', `${path}/him/`],
+  ['Heuse and Zeus', `${path}/heuse-and-zeus/`],
   ['Hombres G', `${path}/hombres-g/`],
   ['Hans Zimmer', `${path}/hans-zimmer-ost/`],
   ['Hozier', `${path}/hozier-band/`],
